ac71b85e533da72d09d2dfa5dc991bbe727070c8
SimonaFilimon <rollysimo6464@gmail.com>
2024-06-11 20:00:04 +0300

    Proiect TA

--- TestBookExpress.py
+++ TestBookExpress.py
@@ -1,7 +1,9 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+
 
 
 class TestBookExpress(unittest.TestCase):
@@ -9,43 +11,35 @@ class TestBookExpress(unittest.TestCase):
         self.driver=webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.books-express.ro/")
+        self.driver.find_element(By.ID, "cookieconsent:desc").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
     def tearDown(self):
         self.driver.quit()
     def test_search_product_sectiune(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[class='category-menu']").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_products_top(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
+    def test_search_products_topvanzari(self):
         self.driver.find_element(By.CSS_SELECTOR, "[class='category-menu']").click()
         self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Top vanzari")
         self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_products_autor(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
+    def test_search_products_autorSethGodin(self):
         self.driver.find_element(By.CSS_SELECTOR, "[class='category-menu']").click()
         self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Top vanzari")
         self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         self.driver.find_element(By.LINK_TEXT, "Seth Godin").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
     def test_search_products_relevanta(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[class='category-menu']").click()
         self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Top vanzari")
-        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
-        self.driver.find_element(By.LINK_TEXT, "Seth Godin").click()
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys(Keys.ENTER)
         self.driver.find_element(By.LINK_TEXT, "Relevanță").click()
         self.driver.find_element(By.LINK_TEXT, "Data publicării").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_products_autor2(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
+    def test_search_products_TheKpiInstitute(self):
         self.driver.find_element(By.CSS_SELECTOR, "[class='category-menu']").click()
-        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Top vanzari")
-        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("Top vanzari2")
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys(Keys.BACK_SPACE)
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys(Keys.ENTER)
         self.driver.find_element(By.LINK_TEXT, "The Kpi Institute").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
--- TestBookExpressSearch.py
+++ TestBookExpressSearch.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 
@@ -10,43 +11,41 @@ class TestBookExpressSearch(unittest.TestCase):
         self.driver=webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.books-express.ro/")
+        self.driver.find_element(By.ID, "cookieconsent:desc").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
     def tearDown(self):
         self.driver.quit()
-    def test_search_product(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
+    def test_search_product_cauta_The_DC_Book(self):
         self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book")
-        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.ENTER)
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_product_click(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
-        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book")
+    def test_search_product_deschide_carte(self):
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book2")
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.BACK_SPACE)
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.ENTER)
         self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[src='https://i3.books-express.ro/bt/9780241506431/the-dc-book.jpg']").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_product_click2(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
-        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book")
-        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    def test_search_product_adauga_in_cos(self):
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book1")
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.BACK_SPACE)
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.ENTER)
         self.driver.find_element(By.CSS_SELECTOR, "[src='https://i3.books-express.ro/bt/9780241506431/the-dc-book.jpg']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[class ='add2cart danger button full icon']").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_product_click3(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
-        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book")
-        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    def test_search_product_autentificare(self):
+        self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book2")
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.BACK_SPACE)
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.ENTER)
         self.driver.find_element(By.CSS_SELECTOR, "[src='https://i3.books-express.ro/bt/9780241506431/the-dc-book.jpg']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[class ='button success']").click()
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
-    def test_search_product_email(self):
-        self.driver.find_element(By.ID, "cookieconsent:desc").click()
-        self.driver.find_element(By.CSS_SELECTOR, "[role='button']").click()
+    def test_search_product_introduce_email(self):
         self.driver.find_element(By.XPATH, "//input[@name='q']").send_keys("the dc book")
         self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[src='https://i3.books-express.ro/bt/9780241506431/the-dc-book.jpg']").click()
         self.driver.find_element(By.CSS_SELECTOR, "[class ='button success']").click()
         self.driver.find_element(By.XPATH, "//input['email']").send_keys("test.@test.t")
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.BACK_SPACE)
+        self.driver.find_element(By.XPATH, "//input['email']").send_keys(Keys.ENTER)
         assert len(self.driver.find_elements(By.CLASS_NAME, "polipop__notifications")) == 2
